95920f9aeea2045f607fefa62920880dafd218f3
Phil Wang <lucidrains@gmail.com>
2023-04-07 09:07:25 -0700

    essential ingredient

--- simple_hierarchical_transformer/simple_hierarchical_transformer.py
+++ simple_hierarchical_transformer/simple_hierarchical_transformer.py
@@ -10,3 +10,35 @@ def exists(val):
     return val is not None
 
 # classes
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8
+    ):
+        super().__init__()
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+        dim_inner = dim_head * heads
+
+        self.to_qkv = nn.Linear(dim, dim_inner * 3, bias = False)
+        self.to_out = nn.Linear(dim_inner, dim, bias = False)
+
+    def forward(self, x):
+        n = x.shape[-2]
+        q, k, v = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), (q, k, v))
+        q = q * self.scale
+
+        sim = einsum('b h i d, b h j d -> b h i j', q, k)
+
+        causal_mask = torch.ones((n, n), device = x.device, dtype = torch.bool).triu(1)
+        sim = sim.masked_fill(causal_mask, -torch.finfo(sim.dtype).max)
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
