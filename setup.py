38d375a7327675c2f8af2855717c042182ac253a
Phil Wang <lucidrains@gmail.com>
2023-04-07 08:34:07 -0700

    scaffold

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'simple-hierarchical-transformer',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Simple Hierarchical Transformer',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/simple-hierarchical-transformer',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'attention mechanism',
+    'hierarchical'
+  ],
+  install_requires=[
+    'einops>=0.4',
+    'torch>=1.6',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
